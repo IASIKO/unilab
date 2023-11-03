dc3b5ac364916de0c16a996bbbc980dfa5bd41d6
IASIKO <iaseshviligi@gmail.com>
2023-11-03 14:24:31 +0400

    add popup

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,24 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navigation.module.css";
 import { Link } from "react-router-dom";
+import Popup from "./Popup";
 
 const Navigation = () => {
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+
   const userImage = localStorage.getItem("userImage");
   const userName = localStorage.getItem("userName");
 
   return (
-    <div className={styles.navWrapper}>
-      <div className={styles.routeContainer}>
-        <Link to="/form">Form</Link>
-        <Link to="/form/api">API</Link>
-      </div>
-      <div className={styles.infoContainer}>
-        <h2>{userName ? userName : "your name"}</h2>
-        <div>
-          <img src={userImage} alt="profile image"/>
+    <>
+      <div className={styles.navWrapper}>
+        <div className={styles.routeContainer}>
+          <Link to="/form">Form</Link>
+          <Link to="/form/api">API</Link>
+        </div>
+        <div className={styles.infoContainer}>
+          <h2>{userName ? userName : "your name"}</h2>
+          <div onClick={() => setIsPopupOpen(true)}>
+            <img src={userImage} alt="profile image" />
+          </div>
         </div>
       </div>
-    </div>
+      {isPopupOpen && (
+        <div
+          className={styles.backdrop}
+          onClick={() => setIsPopupOpen(false)}
+        ></div>
+      )}
+      <Popup isOpen={isPopupOpen} isClose={() => setIsPopupOpen(false)} />
+    </>
   );
 };
 
---
+++ src/components/Popup.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./Popup.module.css";
+import { useNavigate } from "react-router-dom";
+
+const Popup = ({ isOpen, isClose }) => {
+  const navigate = useNavigate();
+
+  const onLogoutClickHandler = () => {
+    localStorage.removeItem("userImage");
+    localStorage.removeItem("userName");
+    navigate("/");
+  };
+
+  return (
+    <div className={`${styles.popupContainer} ${isOpen ? styles.open : ""}`}>
+      <button onClick={isClose}>Close</button>
+      <button onClick={onLogoutClickHandler}>Log out</button>
+    </div>
+  );
+};
+
+export default Popup;
