2a5ba5174077250b3d10eca4347b001ba0547051
IASIKO <iaseshviligi@gmail.com>
2023-11-03 19:42:33 +0400

    update styles and add filter toggle

--- src/components/FormContent.jsx
+++ src/components/FormContent.jsx
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./FormContent.module.css";
 import filter from "../assets/filter.png";
 import search from "../assets/search.png";
 
 const FormContent = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState(false);
+
   return (
     <>
       <div className={styles.bars}>
-        <div className={styles.filter}>
+        <div
+          className={styles.filter}
+          onClick={() => setIsFilterOpen(!isFilterOpen)}
+        >
           <img src={filter} alt="filter icon" />
           <span>filter</span>
         </div>
@@ -15,7 +20,20 @@ const FormContent = () => {
           <input type="search" />
         </div>
       </div>
-      <div className={styles.formList}></div>
+      {isFilterOpen && <div className={styles.filterMenu}></div>}
+      <div className={styles.formList}>
+        <div className={styles.titles}>
+          <p>სტუდეტის სახელი და გვარი</p>
+          <p>სტატუსი</p>
+          <p>სქესი</p>
+          <p>ქულები</p>
+          <p>პირადი ნომერი</p>
+          <p>მეილი</p>
+          <p>ტელეფონი</p>
+          <p>მისამართი</p>
+          <p>დაბადების თარიღი</p>
+        </div>
+      </div>
     </>
   );
 };
