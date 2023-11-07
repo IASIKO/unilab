c539a0cd27134c17ffd33c435046ee908ee77d3c
IASIKO <iaseshviligi@gmail.com>
2023-11-07 15:28:24 +0400

    save

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Registration from "./pages/Registration";
 import Root from "./pages/Root";
@@ -6,33 +6,30 @@ import Form from "./pages/Form";
 import Api from "./pages/Api";
 
 function App() {
-  const router = createBrowserRouter(
-    [
-      {
-        path: "/",
-        element: <Landing />,
-      },
-      {
-        path: "/registration",
-        element: <Registration />,
-      },
-      {
-        path: "/form",
-        element: <Root />,
-        children: [
-          {
-            path: "/form",
-            element: <Form />,
-          },
-          {
-            path: "/form/api",
-            element: <Api />,
-          },
-        ],
-      },
-    ],
-    { basename: "/unilab" }
-  );
+  const router = createHashRouter([
+    {
+      path: "/",
+      element: <Landing />,
+    },
+    {
+      path: "/registration",
+      element: <Registration />,
+    },
+    {
+      path: "/form",
+      element: <Root />,
+      children: [
+        {
+          path: "/form",
+          element: <Form />,
+        },
+        {
+          path: "/form/api",
+          element: <Api />,
+        },
+      ],
+    },
+  ]);
 
   return (
     <>
